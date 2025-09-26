7567b952c14df9d9a97e0d87fa3453ee1b3b5be4
hurto123 <168605839+hurto123@users.noreply.github.com>
2025-09-26 21:53:14 +0700

    Update service-worker.js

--- เว็บคณิตสถิติ/service-worker.js
+++ เว็บคณิตสถิติ/service-worker.js
@@ -34,3 +34,30 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') {
     return;
   }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        if (
+          !networkResponse ||
+          networkResponse.status !== 200 ||
+          !event.request.url.startsWith(self.location.origin)
+        ) {
+          return networkResponse;
+        }
+
+        const responseToCache = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return networkResponse;
+      });
+    })
+  );
+});
